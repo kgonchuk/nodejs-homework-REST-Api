93f3733bc03e6388d0e6bbfa5feb804e2bed117c
kgonchuk <123867028+kgonchuk@users.noreply.github.com>
2023-11-08 13:13:45 +0200

    process.env

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
+const mongoose = require("mongoose");
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const DB_URL =
+  "mongodb+srv://gonchuk:1EDah4vhAoUqa0ax@cluster0.1zdkjik.mongodb.net/Cluster0?retryWrites=true&w=majority";
+
+// const { DB_URL, PORT } = process.env;
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_URL)
+  .then(() => {
+    console.log("database connect succsses");
+    app.listen(300);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
